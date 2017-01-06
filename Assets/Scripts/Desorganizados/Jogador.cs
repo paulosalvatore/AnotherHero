eeb419253f425463ca95e9ea35034489d2f73917
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2017-01-05 22:25:49 -0200

    Diversas alterações no script do Jogador
    Scripts do Jogador/ControladorCena carregados no Awake
    Método de checagem de câmeras que era feito no Update foi passado para
    uma função separada -> Essa função também recebeu uma otimização no
    código
    Métodos de mudança de cena, cenário, definição do destino e alteração da
    posição do jogador após delay adicionados

--- Assets/Scripts/Desorganizados/Jogador.cs
+++ Assets/Scripts/Desorganizados/Jogador.cs
@@ -4,22 +4,41 @@ using System.Collections.Generic;
 
 public class Jogador : MonoBehaviour
 {
+	// Câmeras
 	internal List<GameObject> cameras = new List<GameObject>();
 	internal GameObject[] camerasGO;
 	internal List<Camera> camerasDisponiveis = new List<Camera>();
 
-	internal bool mudandoCena = false;
+	// Cena/Cenário
+	private ControladorCena controladorCena;
+	private bool mudandoCena = false;
+	private bool mudandoCenario = false;
+	private GameObject destino;
+
+	// Jogador
+	internal JogadorMovimento movimentoScript;
+	internal JogadorAnimation animationScript;
+
+	void Awake()
+	{
+		movimentoScript = GetComponent<JogadorMovimento>();
+		animationScript = GetComponent<JogadorAnimation>();
+		controladorCena = ControladorCena.Pegar();
+	}
 
 	void Update()
 	{
+		AtivarCameraDisponivel();
+	}
+
+	void AtivarCameraDisponivel()
+	{
+		if (camerasGO == null || camerasGO.Length == 0 || camerasDisponiveis.Count == 0)
+			return;
+
 		foreach (GameObject camera in camerasGO)
-		{
-			if (camerasDisponiveis.Count > 0)
-			{
-				if (camera && camerasDisponiveis[0])
-					camera.GetComponent<Camera>().enabled = (camerasDisponiveis[0].gameObject == camera ? true : false);
-			}
-		}
+			if (camera && camerasDisponiveis[0])
+				camera.GetComponent<Camera>().enabled = (camerasDisponiveis[0].gameObject == camera ? true : false);
 	}
 
 	public void AtualizarCamerasCenario()
@@ -33,5 +52,76 @@ public class Jogador : MonoBehaviour
 	{
 		camerasGO = new GameObject[0];
 		camerasDisponiveis = new List<Camera>();
+	}
+
+	public void IniciarMudancaCena(string saida, int cenaDestinoId)
+	{
+		if (mudandoCena)
+			return;
+
+		mudandoCena = true;
+
+		movimentoScript.AlterarMovimento(false);
+		
+		controladorCena.CarregarCena(cenaDestinoId);
+		controladorCena.saida = saida;
+
+		LimparCameras();
+	}
+
+	public void FinalizaMudancaCena()
+	{
+		movimentoScript.AlterarMovimento(true);
+
+		AtualizarCamerasCenario();
+
+		controladorCena.saida = null;
+
+		mudandoCena = false;
+	}
+
+	void DefinirDestino(GameObject saida)
+	{
+		GameObject[] checarSaidas = GameObject.FindGameObjectsWithTag("Saída");
+		foreach (GameObject checarSaida in checarSaidas)
+			if (checarSaida.name == saida.name && saida != checarSaida)
+				destino = checarSaida;
+	}
+
+	public void IniciarMudancaCenario(GameObject saida)
+	{
+		if (mudandoCenario)
+			return;
+
+		mudandoCenario = true;
+
+		movimentoScript.AlterarMovimento(false);
+
+		DefinirDestino(saida);
+
+		StartCoroutine(AlterarPosicaoJogadorAposDelay());
+	}
+
+	void FinalizarMudancaCenario()
+	{
+		if (mudandoCenario)
+			return;
+
+		mudandoCenario = false;
+
+		movimentoScript.AlterarMovimento(true);
+	}
+
+	IEnumerator AlterarPosicaoJogadorAposDelay()
+	{
+		controladorCena.Fade("fadeOut");
+
+		yield return new WaitForSeconds(controladorCena.delayCarregamentoCena);
+		
+		transform.position = destino.transform.position;
+
+		FinalizarMudancaCenario();
+
+		controladorCena.Fade("fadeIn");
 	}
 }

7535971743006142964f34e1043718d7fa055dfa
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2017-01-05 22:23:39 -0200

    Melhorias e Modificações no Controlador de Cenário
    Lógica que estava sendo declarada no update foi separada em duas funções
    específicas
    Lógica da finalização da troca de cenário foi movida para o script do
    jogador

--- Assets/Scripts/Controladores/ControladorCenario.cs
+++ Assets/Scripts/Controladores/ControladorCenario.cs
@@ -11,6 +11,7 @@ public class ControladorCenario : MonoBehaviour
 	[Header("Jogador")]
 	public GameObject spawnPointJogador;
 	private GameObject jogador;
+	private bool jogadorInstanciado = false;
 
 	void Start ()
 	{
@@ -19,34 +20,51 @@ public class ControladorCenario : MonoBehaviour
 
 	void Update ()
 	{
-		if (!jogador)
+		ProcurarJogador();
+	}
+
+	void ProcurarJogador()
+	{
+		if (jogadorInstanciado)
+			return;
+
+		jogador = !jogador ? GameObject.Find("Jogador") : null;
+		if (jogador)
+			InstanciarJogador();
+	}
+
+	void InstanciarJogador()
+	{
+		jogadorInstanciado = true;
+		// Desativa efeitos da gravidade ao jogador
+		jogador.GetComponent<Rigidbody>().isKinematic = false;
+
+		// Verifica se o jogador vem de outra cena, caso venha, procurar o local por onde ele saiu
+		// Caso encontre um local de mesmo nome na nova cena, o spawnPoint passa a ser esse novo local
+		// Caso não encontre, o destino será o spawnPoint do Jogador
+		GameObject destino = spawnPointJogador;
+		if (controladorCena.saida != null)
 		{
-			jogador = GameObject.Find("Jogador");
-			if (jogador)
-			{
-				jogador.GetComponent<Rigidbody>().isKinematic = false;
-
-				GameObject destino = spawnPointJogador;
-				if (controladorCena.saida != null)
-				{
-					GameObject procurarDestino = GameObject.Find(controladorCena.saida);
-
-					if (procurarDestino)
-						destino = procurarDestino.transform.FindChild("Spawn").gameObject;
-				}
-
-				jogador.transform.position = new Vector3(
-					destino.transform.position.x,
-					Mathf.Max(jogador.transform.position.y, destino.transform.position.y),
-					destino.transform.position.z
-				);
-
-				jogador.transform.rotation = destino.transform.rotation;
-
-				controladorCena.jogadorMovimentoScript.AlterarMovimento(false);
-				controladorCena.jogadorScript.AtualizarCamerasCenario();
-				controladorCena.jogadorScript.mudandoCena = false;
-			}
+			GameObject procurarDestino = GameObject.Find(controladorCena.saida);
+			if (procurarDestino)
+				destino = procurarDestino.transform.FindChild("Spawn").gameObject;
 		}
+
+		// Aplica a nova posição baseada na posição do destino
+		jogador.transform.position = new Vector3(
+			destino.transform.position.x,
+			Mathf.Max(jogador.transform.position.y, destino.transform.position.y),
+			destino.transform.position.z
+		);
+
+		// Aplicar apenas a rotação do 'destino' no eixo Y.
+		jogador.transform.rotation = new Quaternion(
+			jogador.transform.rotation.x,
+			destino.transform.rotation.y,
+			jogador.transform.rotation.z,
+			jogador.transform.rotation.w
+		);
+
+		controladorCena.jogadorScript.FinalizaMudancaCena();
 	}
 }

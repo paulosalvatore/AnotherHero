e4e5126f995cff8cd9e2458734b6ea80ffe3d2bb
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2017-01-14 10:17:46 -0200

    Script 'LensFlares\TorchLight' adicionado

---
+++ Assets/Packages/Lens Flares/TorchLight.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(Light))]
+public class TorchLight : MonoBehaviour
+{
+
+    public float MinLightIntensity = 0.6F;
+    public float MaxLightIntensity = 1.0F;
+
+    public float AccelerateTime = 0.15f;
+
+    private float _targetIntensity = 1.0f;
+    private float _lastIntensity = 1.0f;
+
+    private float _timePassed = 0.0f;
+
+    private Light _lt;
+    private const double Tolerance = 0.0001;
+
+    private void Start()
+    {
+        _lt = GetComponent<Light>();
+        _lastIntensity = _lt.intensity;
+        FixedUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        _timePassed += Time.deltaTime;
+        _lt.intensity = Mathf.Lerp(_lastIntensity, _targetIntensity, _timePassed / AccelerateTime);
+
+        if (Math.Abs(_lt.intensity - _targetIntensity) < Tolerance)
+        {
+            _lastIntensity = _lt.intensity;
+            _targetIntensity = Random.Range(MinLightIntensity, MaxLightIntensity);
+            _timePassed = 0.0f;
+        }
+    }
+}

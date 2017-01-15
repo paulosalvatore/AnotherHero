15e9183b445407f0bb1e966e2b62155af23dc2bf
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2017-01-15 12:47:48 -0200

    Modificaçoes no script de inventário do jogador
    Drop de item adicionado
    Coleta com a mão direita adicionada
    Interação com itens adicionada

--- Assets/Scripts/Jogador/JogadorInventario.cs
+++ Assets/Scripts/Jogador/JogadorInventario.cs
@@ -8,6 +8,8 @@ public class JogadorInventario : MonoBehaviour
 	private GameObject maoDireita;
 
 	internal GameObject itemDisponivel;
+	internal GameObject interacaoDisponivel;
+	internal GameObject interacaoDestino;
 
 	void Start()
 	{
@@ -17,29 +19,76 @@ public class JogadorInventario : MonoBehaviour
 	
 	void Update()
 	{
-		if (itemDisponivel)
+		if (!interacaoDisponivel && itemDisponivel)
 		{
 			if (Input.GetButtonDown("LB"))
 			{
-				Debug.Log("LB Pressed");
 				ColetarItem("esquerda");
 			}
 			else if (Input.GetButtonDown("RB"))
 			{
-				Debug.Log("RB Pressed");
 				ColetarItem("direita");
 			}
 		}
+		else if (interacaoDisponivel)
+		{
+			if (Input.GetButton("LB") && Input.GetButton("RB"))
+			{
+				IniciarInteracao();
+			}
+		}
+
+		if (Input.GetAxisRaw("LT") == 1 && maoEsquerda.transform.childCount > 0)
+		{
+			DroparItem(maoEsquerda.transform.GetChild(0));
+		}
+		else if (Input.GetAxisRaw("RT") == 1 && maoDireita.transform.childCount > 0)
+		{
+			DroparItem(maoDireita.transform.GetChild(0));
+		}
+	}
+
+	void IniciarInteracao()
+	{
+		if (maoEsquerda.transform.childCount == 0 && maoDireita.transform.childCount == 0)
+		{
+			interacaoDestino.transform.position = new Vector3(
+				interacaoDestino.transform.position.x,
+				12f,
+				interacaoDestino.transform.position.z
+			);
+
+			interacaoDisponivel.GetComponent<Alavanca>().interacaoDisponivel = false;
+
+			interacaoDisponivel = null;
+
+			Debug.Log("Alavanca Pressionada e Desativada. Portão Aberto.");
+		}
+		else
+			Debug.Log("Eu preciso das duas mãos para fazer isso.");
+	}
+
+	void DroparItem(Transform item)
+	{
+		Debug.Log("Dropar item " + item.name + ".");
+
+		item.parent = null;
+		item.position = transform.position;
 	}
 
 	void ColetarItem(string mao)
 	{
 		// Tocar Animação
 
+		if (maoEsquerda.transform.childCount > 0)
+			DroparItem(maoEsquerda.transform.GetChild(0));
+
+		Debug.Log("Coleta item " + itemDisponivel.name);
+
+		itemDisponivel.transform.parent = maoEsquerda.transform;
+
 		if (mao == "esquerda")
 		{
-			itemDisponivel.transform.parent = maoEsquerda.transform;
-
 			itemDisponivel.transform.localPosition = new Vector3(
 				0.0811f,
 				0.0504f,
@@ -48,7 +97,13 @@ public class JogadorInventario : MonoBehaviour
 		}
 		else if (mao == "direita")
 		{
-
+			itemDisponivel.transform.localPosition = new Vector3(
+				0,
+				0,
+				0
+			);
 		}
+
+		itemDisponivel = null;
 	}
 }

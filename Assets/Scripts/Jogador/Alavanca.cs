1a1a6bf273228d59254765a17760d5dcc3146888
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2017-01-15 12:45:52 -0200

    Alavanca adicionada
    Modificações no prefab
    Criação do script que marca a interação com o item disponível

---
+++ Assets/Scripts/Jogador/Alavanca.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alavanca : MonoBehaviour
+{
+	public GameObject portao;
+	internal bool interacaoDisponivel = true;
+
+	private ControladorCena controladorCena;
+
+	void Update()
+	{
+		if (controladorCena == null)
+			controladorCena = ControladorCena.Pegar();
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (!interacaoDisponivel)
+			return;
+
+		if (collider.CompareTag("Player"))
+		{
+			Debug.Log("LB + RB = interact");
+			controladorCena.jogadorScript.inventarioScript.interacaoDisponivel = gameObject;
+			controladorCena.jogadorScript.inventarioScript.interacaoDestino = portao;
+		}
+	}
+
+	void OnTriggerExit(Collider collider)
+	{
+		if (!interacaoDisponivel)
+			return;
+
+		if (collider.CompareTag("Player"))
+		{
+			if (controladorCena.jogadorScript.inventarioScript.interacaoDisponivel == gameObject)
+				controladorCena.jogadorScript.inventarioScript.interacaoDisponivel = null;
+		}
+	}
+}
